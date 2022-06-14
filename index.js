d0400ab2e52e08c99679a4e8a8d2fd9f248c0745
FIRST_NAME LAST_NAME <mustakahammed891@gmail.com>
2022-06-14 20:50:20 +0530

    cors error

--- index.js
+++ index.js
@@ -10,9 +10,26 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 
-app.use(cors({ origin: 'https://second-hackathon.netlify.app'}));
-
 app.use(express.json());
+
+app.use(cors({ origin: ['https://second-hackathon.netlify.app'],
+methods: ["GET", "POST"],
+credentials: true    
+}));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://second-hackathon.netlify.app"); 
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  res.setHeader('content-type', 'application/json');
+  next();
+});
 
 // connection for monogoDB and nodejs
 const Mongo_Url = process.env.Mongo_Url;
